2e0856e80c2243a73d737a12afd819a1465b8ace
TungNguyen <tungnguyenviet23062003@gmail.com>
2025-09-26 10:33:06 +0700

    test photon transform view

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,23 +7,28 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
 
+    private PhotonView photonView;
     private PlayerInput input;
     public Vector3 CurrentPosition => transform.position;
 
     void Awake()
     {
         input = GetComponent<PlayerInput>();
+        photonView = GetComponent<PhotonView>();
     }
 
     void Update()
     {
         if (input != null)
         {
-            Vector3 move = new Vector3(input.MovementInput.x, input.MovementInput.y, 0);
-            transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+            if (photonView.IsMine)
+            {
+                Vector3 move = new Vector3(input.MovementInput.x, input.MovementInput.y, 0);
+                transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
 
-            if (move != Vector3.zero)
-                transform.up = move;
+                if (move != Vector3.zero)
+                    transform.up = move;
+            }
         }
     }
 }
